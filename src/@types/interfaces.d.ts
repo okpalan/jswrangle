3e944f8be05bbee277e6c87bd22258b895a00f92
Nnamdi Michael Okpala <okpalan86@gmail.com>
2022-09-14 12:42:49 +0100

    a

---
+++ src/@types/interfaces.d.ts
@@ -0,0 +1,85 @@
+// treat this file as a module.
+export { }
+
+declare type Token = string | number;
+declare type TokenList = Token[];
+
+declare enum TokenKeyword {
+    LET = 'let',
+    FUNCTION = 'fn',
+    TRUE = 'true',
+    FALSE = 'false',
+    IF = 'if',
+    ELSE = 'else',
+    RETURN = 'return'
+}
+declare enum TokenOperator {
+    PLUS = '+',
+    MINUS = '-',
+    ASTERISK = '*',
+    SLASH = '/',
+    LT = '<',
+    GT = '>',
+    EQ = '==',
+    NOT_EQ = '!=',
+    ASSIGN = '='
+}
+declare enum TokenDelimiter {
+    COMMA = ',',
+    SEMICOLON = ';',
+    LPAREN = '(',
+    RPAREN = ')',
+    LBRACE = '{',
+    RBRACE = '}',
+    LBRACKET = '[',
+    RBRACKET = ']'
+}
+declare enum TokenEOF {
+    EOF = 'EOF'
+}
+
+declare enum TokenETC {
+    SHEBANG = '#!',
+    COMMENT = '//',
+    IDENT = '\t',
+}
+
+declare enum TokenType {
+    Keyword = 'TokenKeyword',
+    Operator = 'TokenOperator',
+    Delimiter = 'TokenDelimiter',
+    ETC = 'TokenETC',
+    EOF = 'TokenEOF'
+}
+
+declare interface ITokenIterable {
+    [Symbol.iterator](): ITokenIterator;
+    [index: number]: Token;
+}
+
+declare interface ITokenIterator {
+    next(): ITokenResult;
+}
+
+declare interface ITokenResult {
+    value: Token;
+    done: boolean;
+}
+
+declare interface ILexer extends ITokenIterable, ITokenIterator {
+    token: Token;
+    pop(): Token;
+    peek(): Token;
+    isEmpty(): boolean;
+}
+
+declare interface ILexerConstructor {
+    constructor(tokens: TokenList): ILexer;
+    new(tokens: TokenList): ILexer;
+    prototype: ILexer;
+}
+
+declare var Lexer: ILexerConstructor;
+
+declare interface IParser {
+}
---
+++ src/classes/Lexer.ts
@@ -0,0 +1,1 @@
+/// <reference path="../@types/interfaces.d.ts"/>

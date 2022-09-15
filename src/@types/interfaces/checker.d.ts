31879057e8b8cdad961ac91ada147b4479bf4d9e
Nnamdi Michael Okpala <okpalan86@gmail.com>
2022-09-15 16:35:41 +0100

    src/@types/checker changed

--- src/@types/interfaces/checker.d.ts
+++ src/@types/interfaces/checker.d.ts
@@ -1,9 +1,11 @@
 /// <reference path="./tokenizer.d.ts"/>
-export {};
+export { };
 /// write a checker interface and types using ./parser.d.ts and ./tokenizer.d.ts.
 
 export declare interface IChecker {
-    isTokenBang(token:Token.IToken): token is Token.ITokenBang,    
-    isTokenKeyword(token:Token.IToken): token is Token.ITokenKeyword,
-    isTokenDelimiter(token:Token.IToken): token
+    isTokenBang(token: Token.IToken): token is Token.ITokenBang,
+    isTokenKeyword(token: Token.IToken): token is Token.ITokenKeyword,
+    isTokenDelimiter(token: Token.IToken): token is Token.ITokenDelimiter,
+    isTokenOperator(token: Token.IToken): token is Token.ITokenOperator,
+    isTokenEnd(token: Token.IToken): token is Token.ITokenEnd,
 }
